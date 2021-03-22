e0804325e4f9b80e4c1f661599d4dff6160ac7c5
haavarty <haavarty@stud.ntnu.no>
2021-03-22 14:37:56 +0100

    fix punktum shit

--- 4/kalkulator/src/components/Kalkulator.vue
+++ 4/kalkulator/src/components/Kalkulator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="kalkulator">
-  <div class="display">{{regneStykke || 0}}</div>
+    <div class="display">{{ regneStykke || 0 }}</div>
     <div class="kalkulator-knapper">
       <button class="kalkulator-knapp er-clear" @click="clear">C</button>
       <button class="kalkulator-knapp" @click="leggTilForgjeSvar">ANS</button>
@@ -18,115 +18,121 @@
       <button class="kalkulator-knapp" @click="add(8)">8</button>
       <button class="kalkulator-knapp" @click="add(9)">9</button>
       <button class="kalkulator-knapp" @click="add('/')">&divide;</button>
-      <button class="kalkulator-knapp" ></button>
+      <button class="kalkulator-knapp"></button>
       <button class="kalkulator-knapp" @click="add(0)">0</button>
       <button class="kalkulator-knapp" @click="add('.')">.</button>
-      <button class="kalkulator-knapp er-lik" @click="calculate">&equals;</button>
+      <button class="kalkulator-knapp er-lik" @click="calculate">
+        &equals;
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        regneStykke: '',
-        forgjeSvar: null
-      }
+  data() {
+    return {
+      regneStykke: "",
+      forgjeSvar: null,
+    };
+  },
+  methods: {
+    clear() {
+      this.regneStykke = "";
     },
-    methods: {
-      clear() {
-        this.regneStykke = '';
-      },
-      add(addedValue) {
-        this.regneStykke += addedValue;
-      },
-      deleteOne() {
-        let tempVerdi = this.regneStykke.slice(0, -1);
-        this.regnestykke = '';
-        this.regneStykke = tempVerdi;
-      },
-      calculate() {
-         let tempVerdi= eval(this.regneStykke);
-         let loggInfo = this.regneStykke + " = " + tempVerdi;
-         this.$emit('legg-til-logg', loggInfo);
-         this.regneStykke = tempVerdi;
-         this.forgjeSvar = tempVerdi;
-      },
-      leggTilForgjeSvar(){
-        if(this.forgjeSvar != null) {
-          this.regneStykke += this.forgjeSvar
+    add(addedValue) {
+      this.regneStykke += addedValue;
+    },
+    deleteOne() {
+      let tempVerdi = this.regneStykke.slice(0, -1);
+      this.regnestykke = "";
+      this.regneStykke = tempVerdi;
+    },
+    calculate() {
+      try {
+          let tempVerdi = eval(this.regneStykke);
+          let loggInfo = this.regneStykke + " = " + tempVerdi;
+          this.$emit("legg-til-logg", loggInfo);
+          this.regneStykke = tempVerdi;
+          this.forgjeSvar = tempVerdi;
+      } catch(e) {
+        if(e instanceof SyntaxError) {
+          alert(e.message);
         }
       }
-    }
+    },
+    leggTilForgjeSvar() {
+      if (this.forgjeSvar != null) {
+        this.regneStykke += this.forgjeSvar;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 * {
   box-sizing: border-box;
 }
 
 body {
-  padding-bottom:30px;
-  padding-top:30px;
+  padding-bottom: 30px;
+  padding-top: 30px;
 }
 
 .kalkulator {
-    max-width: 500px;
-    margin: 0 auto;
-    border: 2px solid #111;
-    border-radius: 5px;
-    background:#333333;
+  max-width: 500px;
+  margin: 0 auto;
+  border: 2px solid #111;
+  border-radius: 5px;
+  background: #333333;
 }
 
-
-.kalkulator-knapper{
-  padding:20px;
-  display:grid;
-  grid-template-columns:repeat(4, 1fr);
-  grid-gap:3px;
+.kalkulator-knapper {
+  padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 3px;
 }
 
 .display {
-    background: dimgray;
-    color:white;
-    padding: 10px;
-    width: 100%;
-    border: 1px solid #111;
-    text-align: center;
-    font-size: 40px;
-    border-radius: 0;
-    height:70px;
+  background: dimgray;
+  color: white;
+  padding: 10px;
+  width: 100%;
+  border: 1px solid #111;
+  text-align: center;
+  font-size: 40px;
+  border-radius: 0;
+  height: 70px;
 }
 
 .kalkulator-knapp {
-  font-size:30px;
+  font-size: 30px;
   background: dimgray;
-  color:white;
-  width:100px;
-  height:60px;
+  color: white;
+  width: 100px;
+  height: 60px;
   border-radius: 5%;
-  cursor:pointer;
-  border: 1px solid #111; 
-  text-align:center;
+  cursor: pointer;
+  border: 1px solid #111;
+  text-align: center;
 }
 
 .er-lik {
-  background:orange;
+  background: orange;
 }
 
 .er-clear {
-   background:green;
+  background: green;
 }
 
 input[type="number"]::-webkit-outer-spin-button,
 input[type="number"]::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+  -webkit-appearance: none;
+  margin: 0;
 }
 input[type="number"] {
-    -moz-appearance: textfield;
+  -moz-appearance: textfield;
 }
 </style>
